fe10f5319059b4b56692abfa8293b9069200b528
Dichlorocarbene <88939959+Dichlorocarbene@users.noreply.github.com>
2022-01-17 14:09:07 +0800

    support some CQ code

--- msg_part.py
+++ msg_part.py
@@ -1,12 +1,13 @@
 import requests as r
 import constants as c
-
+import warnings as w
 # group message
 
 
 def send_group_message(group_id: int, content: str):
     link = c.group_msg + "group_id=" + str(group_id) + "&message=" + content
     print(link)
+    __len_check(link)
     t = r.post(url=link)
     return t
 
@@ -14,5 +15,16 @@ def send_group_message(group_id: int, content: str):
 def send_private_message(user_id: int, content: str):
     link = c.private_msg + "user_id=" + str(user_id) + "&message=" + content
     print(link)
+    __len_check(link)
     t = r.post(url=link)
     return t
+
+
+def __len_check(st):
+    if len(st) > 1000:
+        w.warn("sent message too long")
+
+
+def cq_pic(filepath):
+    pic = "[CQ:image,file=" + c.working_path + filepath + "]"
+    return pic
